dfaad1121a1c622394b257838017b25c8b239f24
ARLIKIN <nik5552000@mail.ru>
2024-04-15 23:06:08 +0400

    Сделал заглушки ViewSet для всех эндпоинтов

--- backend/foodgram/api/urls.py
+++ backend/foodgram/api/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
-from rest_framework.routers import DefaultRouter
+from rest_framework.routers import SimpleRouter
+
+router_v1 = SimpleRouter()
 
 
 urlpatterns = [
--- backend/foodgram/api/views.py
+++ backend/foodgram/api/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from django.contrib.auth import get_user_model
+
+from backend.foodgram.food.models import (
+    Tag, Recipe,
+    Favorite, Ingredient,
+    ShoppingCart, Subscribe
+)
+
+User = get_user_model()
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+
+
+class TegViewSet(viewsets.ModelViewSet):
+    queryset = Tag.objects.all()
+
+
+class RecipeViewSet(viewsets.ModelViewSet):
+    queryset = Recipe.objects.all()
+
+
+class ShoppingCartViewSet(viewsets.ModelViewSet):
+    queryset = ShoppingCart.objects.all()
+
+
+class FavoriteViewSet(viewsets.ModelViewSet):
+    queryset = Favorite.objects.all()
+
+
+class SubscribeViewSet(viewsets.ModelViewSet):
+    queryset = Subscribe.objects.all()
+
+
+class IngredientsViewSet(viewsets.ModelViewSet):
+    queryset = Ingredient.objects.all()
