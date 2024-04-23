229f66b34e87ccf3689ca1893528c4c48db2f0db
ARLIKIN <nik5552000@mail.ru>
2024-04-23 20:48:14 +0400

    Настроил отображение изображений

--- backend/foodgram/foodgram/urls.py
+++ backend/foodgram/foodgram/urls.py
@@ -1,6 +1,9 @@
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+
+from .settings import MEDIA_URL, MEDIA_ROOT
 
 router = DefaultRouter()
 
@@ -8,3 +11,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
 ]
+
+urlpatterns += static(MEDIA_URL, document_root=MEDIA_ROOT)
